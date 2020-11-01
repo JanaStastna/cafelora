782c524b8a5746ab84dfe622ff4fc9be424d69a5
Jana Stastna <j.machkova@gmail.com)>
2020-11-01 12:13:02 +0100

    funkce navigace hamburger

--- cafelora-zadani-main/src/index.js
+++ cafelora-zadani-main/src/index.js
@@ -2,3 +2,8 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const btnElm = document.querySelector('#nav-btn');
+btnElm.addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
+});
