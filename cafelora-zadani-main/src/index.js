850520598a45c4db8ffebae3bd30bff7ec15d8d4
Jana Stastna <j.machkova@gmail.com)>
2020-11-01 12:55:14 +0100

    zavirani navigace hamburger po kliknuti

--- cafelora-zadani-main/src/index.js
+++ cafelora-zadani-main/src/index.js
@@ -3,7 +3,16 @@ import './style.css';
 
 console.log('funguju!');
 
+const nav = document.querySelector('nav');
+
 const btnElm = document.querySelector('#nav-btn');
 btnElm.addEventListener('click', () => {
-  document.querySelector('nav').classList.toggle('nav-closed');
+  nav.classList.toggle('nav-closed');
+});
+
+const navElm = document.querySelectorAll('.navigace');
+navElm.forEach((polozka) => {
+  polozka.addEventListener('click', () => {
+    nav.classList.add('nav-closed');
+  });
 });
